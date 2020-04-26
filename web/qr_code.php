3dcd6ad910a4ac9e55c5a17c5d906b4beb5f1c36
I.A.Gzyk <ivangzyk@hotmail.com>
2020-04-26 20:42:11 -0300

    Instalado composer

--- web/qr_code.php
+++ web/qr_code.php
@@ -1,15 +1,19 @@
 <?php
+include_once '/vendor/autoload.php';
 
 class qr_code {
 
     function gera_qr_code() {
-		
-	$codigo = base64_encode(sha1(md5('13.457.853/0001-07')));
-    $cnpj = '13.457.853/0001-07';
+
+        $mpdf = new \Mpdf\Mpdf();
+        $mpdf->WriteHTML('https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=http://cmanager.com.br/web/login/cadastrar.php?codigo=' . $codigo . '&cnpj=' . $cnpj . '');
+        $mpdf->Output();
+
+        $codigo = base64_encode(sha1(md5('13.457.853/0001-07')));
+        $cnpj = '13.457.853/0001-07';
         //echo "https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=http%3A%2F%2Fwww.google.com%2F&choe=UTF-8";
-    header('Location:https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=http://cmanager.com.br/web/login/cadastrar.php?codigo=' . $codigo . '&cnpj=' . $cnpj . '');
+        //header('Location:https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=http://cmanager.com.br/web/login/cadastrar.php?codigo=' . $codigo . '&cnpj=' . $cnpj . '');
     }
 
 }
-
 ?>
---
+++ web/script/qr_code.php
@@ -0,0 +1,5 @@
+<?php
+include_once '../qr_code.php';
+$code = new qr_code();
+$qr = $code->gera_qr_code();
+

dd8602c2e686319fac7f14921d8b557e9acb5b15
Cleverson <clevinhoirineu@hotmail.com>
2020-04-26 01:12:46 -0300

    Nova Versão 26/04/2020 01:12 - Atualizado função do QRCode para cadastro. - Corrigido Menu Responsivo para acesso ao celular.

--- controllers/menucontroller.php
+++ controllers/menucontroller.php
@@ -7,13 +7,15 @@ class MenuController {
         $db = new Conexao();
         $con = $db->con;
 
-        $menu = "<nav class = 'navbar navbar-expand-lg py-3 navbar-dark bg-primary shadow-sm'>
+        $menu = "<nav class = 'navbar navbar-expand-lg py-3 navbar-static-top navbar-light' style = 'background-color: #e3f2fd;'>
         <a href = 'index.php' class = 'navbar-brand'>
         <img src = 'img/logo-cmanager-pequeno.png' width = '45' alt = '' class = 'd-inline-block align-middle mr-2'>
         <span class = 'text-uppercase font-weight-bold'>CMANAGER</span>
         </a>
-
-        <div id = 'navbarSupportedContent' class='collapse navbar-collapse'>
+		
+   		<button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarTogglerDemo01' aria-controls='navbarTogglerDemo01' aria-expanded='false' aria-label='Toggle navigation'><span class='navbar-toggler-icon'></span></button>
+				  
+        <div class='collapse navbar-collapse' id='navbarTogglerDemo01'>
         <ul class = 'navbar-nav ml-auto'>
         <li class = 'nav-item active'>
         <ul class='navbar-nav mr-auto'>";
--- web/qr_code.php
+++ web/qr_code.php
@@ -3,8 +3,13 @@
 class qr_code {
 
     function gera_qr_code() {
+		
+	$codigo = base64_encode(sha1(md5('13.457.853/0001-07')));
+    $cnpj = '13.457.853/0001-07';
         //echo "https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=http%3A%2F%2Fwww.google.com%2F&choe=UTF-8";
-        header('Location:https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=http://cmanager.com.br');
+    header('Location:https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=http://cmanager.com.br/web/login/cadastrar.php?codigo=' . $codigo . '&cnpj=' . $cnpj . '');
     }
 
 }
+
+?>

db6aa556c1baae7cf8cfee031a3903f4e50f74c1
I.A.Gzyk <ivangzyk@hotmail.com>
2020-04-26 10:35:15 -0300

    arrumando relaorio Usuario

--- views/usuario/form_update.php
+++ views/usuario/form_update.php
@@ -25,27 +25,23 @@ print_r($user);
         <div class="form-group">
             Nome:<input type="text" class="form-control" name="nome" value="<?= $user['nome'] ?>">
         </div>
-        <?php if($tipo == 0){ ?>
-        <div class="form-group">
-            Tipo:
-            <select name="tipo" class="form-control">
-                <option value='0'>Condomino</option>
-            </select>
-        </div>        
-        <?php }else{ ?>
         <div class="form-group">
             Tipo:
             <select name="tipo" class="form-control">
                 <?= $opcao ?>
             </select>
-        </div>        
-        <?php } ?>
+        </div>
         <div class="form-group">
             Senha:<input type="password" class="form-control" name="senha" value="<?= $user['senha'] ?>">
         </div>
         <div class="form-group">
-            Ativo:<input type="number" class="form-control" name="ativo" value="<?= $user['tipo'] ?>">
+            Ativo:
+            <select name="ativo" class="form-control">
+                <option value='0'>Não</option>
+                <option value='1'>Sim</option>
+            </select>
         </div>
+        
         <div class="form-group">
             Condominio:
             <select name="tipoCond" class="form-control">

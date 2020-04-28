74322dc6a5dd7224eb32046bffa02e9ce0851875
I.A.Gzyk <ivangzyk@hotmail.com>
2020-04-28 18:21:34 -0300

    cadastro condominio

--- views/condominio/create.php
+++ views/condominio/create.php
@@ -6,24 +6,39 @@ include_once '../../config/conexao.php';
 include '../../controllers/condominiocontroller.php';
 
 $db = new Conexao();
-//print_r($_POST);
-//exit();
 $cond = new CondominioController();
+if (isset($_GET['cond'])) {
+    $condominio = $cond->CreateCondominio($_POST['nome_cond'], $_POST['cnpj']);
+    $endereço = $condominio->setEnderesso($_POST['rua'], $_POST['num'], $_POST['bairro'], $_POST['cidade'], $_POST['estado'], $_POST['cep']);
+    $nome = $condominio->getCondo();
+    $cnpj = $condominio->getCpfCnpj();
+    $rua = $endereço->getRua();
+    $numero = $endereço->getNumero();
+    $bairro = $endereço->getBairro();
+    $cidade = $endereço->getCidade();
+    $estado = $endereço->getEstado();
+    $cep = $endereço->getCep();
+    $query_condominio = "INSERT INTO CadastrCpf_Cnpj (cpf_cnpj, nome) VALUES ('$cnpj', '$nome');";
+    $query_endereco = "INSERT INTO Endereco (cpf_cnpj, Rua, numero, bairro, cidade, estado, cep) VALUES ('$cnpj', '$rua', '$numero', '$bairro', '$cidade', '$estado', '$cep');";
+
+    $execute_1 = mysqli_query($db->con, $query_condominio);
+    $execute_2 = mysqli_query($db->con, $query_endereco);
+    header("Location:http://localhost/cmanager/Web/index.php");
+}
 $condominio = $cond->Create($_POST['nome'], $_POST['salva'], $_POST['apt'], $_POST['blc']);
 ?><pre><?php
 //print_r($condominio);
-    ?></pre><?php
+?></pre><?php
 //exit();
-$nome = $condominio->getCondo();
-$doc = $condominio->getCpfCnpj();
-$apt = $condominio->getApartamento();
-$blc = $condominio->getBloco();
+    $nome = $condominio->getCondo();
+    $doc = $condominio->getCpfCnpj();
+    $apt = $condominio->getApartamento();
+    $blc = $condominio->getBloco();
 
-$query = "INSERT INTO Predio (condominio, blc,ap) VALUES ('$doc', '$blc', '$apt');";
+    $query = "INSERT INTO Predio (condominio, blc,ap) VALUES ('$doc', '$blc', '$apt');";
 
-$execute = mysqli_query($db->con, $query);
+    $execute = mysqli_query($db->con, $query);
 
-header("Location:../../web/index.php?cond=$doc");
-
-?>
+    header("Location:../../web/index.php?cond=$doc");
+    ?>
 

02900bec5e3832bdbae53e661bae80d2e642bad2
I.A.Gzyk <ivangzyk@hotmail.com>
2020-04-30 09:10:41 -0300

    Melhorias no pdf do qr code

--- web/qr_code.php
+++ web/qr_code.php
@@ -14,15 +14,27 @@ class qr_code {
         $url = '<img src="' . (new QRCode)->render($data) . '" />';
         echo $url;
     }
-    
-    function geraPdf(){
+
+    function geraPdf() {
         $codigo = base64_encode(sha1(md5('13.457.853/0001-07')));
         $cnpj = '13.457.853/0001-07';
-        $data = "http://cmanager.com.br/web/login/cadastrar.php?codigo=$codigo&cnpj=$cnpj"; //inserindo a URL
-
-        $url = '<img src="' . (new QRCode)->render($data) . '" />';
-        
+        $data = "http://cmanager.com.br/web/login/cadastrar.php?codigo=$codigo&cnpj=$cnpj"; //inserindo a URL        
+        $url = '<div  align="center"><img src="' . (new QRCode)->render($data) . '" /></div>';
+        $stylesheet = file_get_contents('css/dataTables.bootstrap4.min.css');
         $mpdf = new \Mpdf\Mpdf();
+        $mpdf->SetHeader('<div style="text-align: center; font-weight: bold; font-size: 2em;">
+    QrCode para cadastro de Condomino.
+</div>','O');
+        $mpdf->SetFooter('<footer class="py-3 bg-light mt-auto">
+                    <div class="container-fluid">
+                        <div class="d-flex align-items-end justify-content-end small">
+                            <div class="text-muted">Copyright &copy; 2020 Condominium Manager. Todos os direitos reservados.</div>
+                        </div>
+                    </div>
+                </footer>');
+        $mpdf->SetTitle("CMANAGER");
+        //$mpdf->SetDirectionality('rtl');
+        $mpdf->WriteHTML($stylesheet, \Mpdf\HTMLParserMode::HEADER_CSS);
         $mpdf->WriteHTML($url);
         $mpdf->Output();
     }
